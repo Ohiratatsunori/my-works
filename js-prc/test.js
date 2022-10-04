154b9d046aca5110222861762958a8e901e35381
大平達昌 <rkwcb837@gmail.com>
2022-10-04 23:27:22 +0900

    js作成

---
+++ js-prc/test.js
@@ -0,0 +1,14 @@
+console.log('こんにちわ');
+// alert('ウイルスを検知しました');
+let number = 0;
+const number2 = 0;
+
+function testFunc(){
+  number++;
+  if(number % 3 === 0){
+    //アホになる
+    console.log(number +'ウホッ');
+  } else {
+    console.log(number +'回押された');
+  }
+}
